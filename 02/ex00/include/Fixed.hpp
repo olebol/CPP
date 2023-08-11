9541b5334320492b08c0d933a679dfd65480860e
Olebol - CODAM <ole.pelser@gmail.com>
2023-08-11 23:40:20 +0200

    init 02 ex00

---
+++ 02/ex00/include/Fixed.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Fixed.hpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: opelser <opelser@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/11 17:17:04 by opelser       #+#    #+#                 */
+/*   Updated: 2023/08/11 22:04:35 by opelser       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+
+class Fixed
+{
+	private:
+
+	public:
+		// Constructors
+		Fixed(const Fixed &copy);
+		Fixed(void);
+		~Fixed();
+
+} ;
+
+#endif
---
+++ 02/ex00/src/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: opelser <opelser@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/09 17:26:40 by opelser       #+#    #+#                 */
+/*   Updated: 2023/08/11 23:37:12 by opelser       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Fixed.hpp"
+
+#include <iostream>
+
+int		main(void)
+{
+	std::cout << "Hello world!" << std::endl;
+	return (0);
+}
