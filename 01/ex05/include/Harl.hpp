9310657e266936fa27d281b0a6b995bc2b81ab28
Olebol - CODAM <ole.pelser@gmail.com>
2023-08-11 18:36:48 +0200

    added lemongrab quotes :))))

--- 01/ex05/include/Harl.hpp
+++ 01/ex05/include/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/11 17:17:04 by opelser       #+#    #+#                 */
-/*   Updated: 2023/08/11 17:43:56 by opelser       ########   odam.nl         */
+/*   Updated: 2023/08/11 18:35:42 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,25 +31,6 @@ class Harl
 
 		// Methods
 		void		complain(std::string level);
-};
+} ;
 
 #endif
-
-
-// "DEBUG" level: Debug messages contain contextual information. They are mostly
-// used for problem diagnosis.
-// Example: "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
-
-// • "INFO" level: These messages contain extensive information. They are helpful for
-// tracing program execution in a production environment.
-// Example: "I cannot believe adding extra bacon costs more money. You didn’t put
-// enough bacon in my burger! If you did, I wouldn’t be asking for more!"
-
-// • "WARNING" level: Warning messages indicate a potential issue in the system.
-// However, it can be handled or ignored.
-// Example: "I think I deserve to have some extra bacon for free. I’ve been coming for
-// years whereas you started working here since last month."
-
-// • "ERROR" level: These messages indicate an unrecoverable error has occurred.
-// This is usually a critical issue that requires manual intervention.
-// Example: "This is unacceptable! I want to speak to the manager now."
--- 01/ex05/src/Harl.cpp
+++ 01/ex05/src/Harl.cpp
@@ -6,13 +6,14 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/11 17:25:01 by opelser       #+#    #+#                 */
-/*   Updated: 2023/08/11 18:05:48 by opelser       ########   odam.nl         */
+/*   Updated: 2023/08/11 18:36:24 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 #include <iostream>
 #include <random>
+#include <ctime>
 
 #define C_LRED		"\x1b[38;2;255;128;128m"
 #define C_LBLUE		"\x1b[38;2;128;128;255m"
@@ -35,10 +36,13 @@ Harl::~Harl()
 // Methods
 void		Harl::debug(void)
 {
-	int			random = std::rand();
-	const		std::string		quotes[3] = 
+	int			random = std::rand() % 4;
+	const		std::string		quotes[4] = 
 	{
-		
+		"If anyone needs me, I'll be taking a nap! A-a-and clean this place up or dungeon! Three hours dungeon",
+		"I am within my rights!",
+		"You try to change my ways? You're poison! You're poison! You need reconditioning! Reconditiooooooooon!",
+		"Huh? What? Who's this rigamarole?"
 	} ;
 
 	std::cout << C_LGREEN << C_BOLD << quotes[random] << C_RESET << std::endl;
@@ -46,10 +50,12 @@ void		Harl::debug(void)
 
 void		Harl::info(void)
 {
-	int			random = std::rand();
+	int			random = std::rand() % 3;
 	const		std::string		quotes[3] = 
 	{
-		
+		"Castle Lemongrab has no citizens! You have excess candies! You must donate! Donate!",
+		"Blombo, you must heed my instructions! Take off your thiiings!",
+		"I can't stands no more!"
 	} ;
 
 	std::cout << C_LBLUE << C_BOLD << quotes[random] << C_RESET << std::endl;
@@ -57,21 +63,26 @@ void		Harl::info(void)
 
 void		Harl::warning(void)
 {
-	int			random = std::rand();
+	int			random = std::rand() % 3;
 	const		std::string		quotes[3] = 
 	{
-		
+		"Too young! Too young to rule the kingdom!",
+		"Stop screaming! Why are you screaming?!",
+		"Oh! OOHH! Oh! This sounds awful!!!"
 	} ;
 
-	std::cout << C_LBLUE << C_BOLD << quotes[random] << C_RESET << std::endl;
+	std::cout << C_LYELLOW << C_BOLD << quotes[random] << C_RESET << std::endl;
 }
 
 void		Harl::error(void)
 {
-	int			random = std::rand();
-	const		std::string		quotes[3] = 
+	int			random = std::rand() % 4;
+	const		std::string		quotes[4] = 
 	{
-		
+		"This castle is in unacceptable condition! Unacceptable!",
+		"Twelve years dungeon. All of you. Dungeon. Seven years, no trials. C'mon, let's move it!",
+		"One million years dungeon!",
+		"You serve no function. You must be destroyed."
 	} ;
 
 	std::cout << C_LRED << C_BOLD << quotes[random] << C_RESET << std::endl;
@@ -79,9 +90,26 @@ void		Harl::error(void)
 
 void		Harl::complain(std::string level)
 {
-	void	(Harl::*function[4])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error }; // Pointers to member functions
-	const std::string		levels[4] = { "debug", "info", "warning", "error" }; // All complain levels
+	void	(Harl::*function[4])(void) =
+	{
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	} ; // Pointers to member functions
 
+	const std::string		levels[4] =
+	{
+		"debug",
+		"info",
+		"warning",
+		"error"
+	} ; // All complaint levels
+
+	// Generates a random seed for the rand() function, which gets used to select a random quote
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+	// Checks what complaint level is called
 	for (int i = 0; i < 4; i++)
 	{
 		if (levels[i].compare(level) == 0)
