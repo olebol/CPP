7b4a65560a52831584609e9c8c958d0b7abffd61
olebol <ole.pelser@gmail.com>
2024-01-03 18:15:29 +0100

    Add Bureaucrat class and main function

---
+++ 05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:32:08 by opelser           #+#    #+#             */
+/*   Updated: 2024/01/03 16:56:11 by opelser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREACRAT_HPP
+# define BUREACRAT_HPP
+
+#include <string>
+
+class Bureaucrat
+{
+	private:
+		// Attributes
+		std::string		_name;
+		int				_grade;
+
+	public:
+		// Constructors
+		Bureaucrat(void);
+		Bureaucrat(const std::string &name, const int grade);
+		Bureaucrat(const Bureaucrat &copy);
+		Bureaucrat	&operator=(const Bureaucrat &rhs);
+		~Bureaucrat();
+
+		// Exceptions
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char		*what() const throw();
+		} ;
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char		*what() const throw();
+		} ;
+
+		// Getters - Setters
+		std::string		getName(void) const;
+		int				getGrade(void) const;
+		void			setGrade(const int grade);
+
+		// Member functions
+		void			promote(void);
+		void			demote(void);
+} ;
+
+// Operator overloads
+std::ostream	&operator<<(std::ostream &o, const Bureaucrat &rhs);
+
+#endif
