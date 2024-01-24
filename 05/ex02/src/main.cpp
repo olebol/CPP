83d766bf008b74474c8f37849f913890bde6fc98
Ole Pelser <opelser@f0r3s25.codam.nl>
2024-01-24 15:41:21 +0100

    updated main function structure

--- 05/ex02/src/main.cpp
+++ 05/ex02/src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:26:40 by opelser           #+#    #+#             */
-/*   Updated: 2024/01/24 15:03:02 by opelser          ###   ########.fr       */
+/*   Updated: 2024/01/24 15:40:30 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Bureaucrat.hpp"
 #include "../include/ShrubberyCreationForm.hpp"
 #include "../include/RobotomyRequestForm.hpp"
+#include "../include/PresidentialPardonForm.hpp"
 
 #include <iostream>
 #include <exception>
@@ -21,75 +22,45 @@
 #define RED "\033[31m"
 #define RESET "\033[0m"
 
-int main(void)
+void	testForm(Bureaucrat &jim, Bureaucrat &chad, AForm &form)
 {
-	// Test default behaviour
-	try
+	std::cout << "\nTesting form: " << form.getName() << std::endl;
+	try 
 	{
-		Bureaucrat					jim("Jim", 150);
-		Bureaucrat					bob("Bob", 75);
-		ShrubberyCreationForm		form("trees");
-
-		std::cout << form << std::endl;
-
 		// Too low grade to sign or execute
 		jim.signForm(form);
 		jim.executeForm(form);
 
 		// Not signed
-		bob.executeForm(form);
+		chad.executeForm(form);
 
 		// Sign and execute
-		bob.signForm(form);
-		bob.executeForm(form);
+		chad.signForm(form);
+		chad.executeForm(form);
 	}
 	catch (std::exception &e)
 	{
 		std::cout << RED << "Exception caught: " << e.what() << RESET << std::endl;
 	}
+	std::cout << std::endl;
+}
 
+int	main(void)
+{
 	try
 	{
-		Bureaucrat					bob("Bob", 75);
-		Bureaucrat					jeremy("Jeremy", 40);
-		RobotomyRequestForm			form("robot");
+		Bureaucrat					jim("Jim", 150);
+		Bureaucrat					chad("Chad", 1);
+		// Bureaucrat					kyle("Kyle", 0);
+		// Bureaucrat					brad("Brad", 151);
 
-		std::cout << form << std::endl;
+		ShrubberyCreationForm		shrubbery("house");
+		RobotomyRequestForm			robotomy("human");
+		PresidentialPardonForm		presidential("criminal");
 
-		// Too low grade to sign or execute
-		bob.signForm(form);
-		bob.executeForm(form);
-
-		// Not signed
-		jeremy.executeForm(form);
-		
-		// Sign and execute
-		jeremy.signForm(form);
-		jeremy.executeForm(form);
-
-		// Execute a few times to see randomized
-		for (int i = 0; i < 20; i++)
-			jeremy.executeForm(form);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << RED << "Exception caught: " << e.what() << RESET << std::endl;
-	}
-
-	// Test GradeTooHighException
-	try
-	{
-		Bureaucrat		bob("Bob", 0);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << RED << "Exception caught: " << e.what() << RESET << std::endl;
-	}
-
-	// Test GradeTooLowException
-	try
-	{
-		Bureaucrat		bob("Bob", 151);
+		testForm(jim, chad, shrubbery);
+		testForm(jim, chad, robotomy);
+		testForm(jim, chad, presidential);
 	}
 	catch (std::exception &e)
 	{
