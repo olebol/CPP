762fa3332447e20fe9b41458a41931fbf55a9689
olebol <ole.pelser@gmail.com>
2024-02-01 16:26:05 +0100

    init span

---
+++ 08/ex01/include/Span.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 20:35:53 by opelser           #+#    #+#             */
+/*   Updated: 2024/01/31 20:36:07 by opelser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <string>
+
+class Span
+{
+	private:
+		// Private Attributes
+		
+
+	public:
+		// Constructors
+		Span();
+		Span(const Span &rhs);
+		Span &operator=(const Span &rhs);
+		~Span();
+
+		// Public Methods
+
+} ;
+
+
+#endif
