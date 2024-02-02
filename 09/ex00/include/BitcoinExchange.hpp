64b153c77d7d8e7aace49d65747bb7e646093419
olebol <ole.pelser@gmail.com>
2024-02-02 18:08:05 +0100

    Add BitcoinExchange class constructor that takes a filename as a parameter and parses the data from the file to add rates to the map.

--- 09/ex00/include/BitcoinExchange.hpp
+++ 09/ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 22:22:11 by opelser           #+#    #+#             */
-/*   Updated: 2024/02/01 22:22:27 by opelser          ###   ########.fr       */
+/*   Updated: 2024/02/02 17:44:38 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,48 @@
 
 # include <string>
 
+# include <map>
+
 class BitcoinExchange
 {
 	private:
 		// Private Attributes
-		
+		std::map<std::string, double>		_exchangeRates;
 
 	public:
 		// Constructors and Destructors
 		BitcoinExchange(void);
+		BitcoinExchange(std::string &filename);
 		BitcoinExchange(const BitcoinExchange &rhs);
 		BitcoinExchange &operator=(const BitcoinExchange &rhs);
 		~BitcoinExchange(void);
 
-		// Public Methods
+		// Exceptions
+		class NoRateAvailable : public std::exception
+		{
+			public:
+				virtual const char *what() const noexcept
+				{
+					return ("No rate available for this date");
+				}
+		};
 
+		class invalidValue : public std::exception
+		{
+			public:
+				virtual const char *what() const noexcept
+				{
+					return ("Entered value is not between 0 and 1000");
+				}
+		};
+
+		// Adding rates
+		void		addRate(std::string date, double rate);
+		void		parseData(std::string &filename);
+
+		// Public Methods
+		double		getRate(std::string &date) const;
+		double		exchange(std::string &date, double amount) const;
 } ;
 
 
--- 09/ex00/src/BitcoinExchange.cpp
+++ 09/ex00/src/BitcoinExchange.cpp
@@ -6,13 +6,14 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 22:22:51 by opelser           #+#    #+#             */
-/*   Updated: 2024/02/01 22:23:00 by opelser          ###   ########.fr       */
+/*   Updated: 2024/02/02 18:07:41 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/BitcoinExchange.hpp"
 
 #include <iostream>
+#include <fstream>
 
 #define GREEN "\033[32m"
 #define RED "\033[31m"
@@ -23,8 +24,16 @@
 // ************************************************************************** //
 
 BitcoinExchange::BitcoinExchange(void)
+	:	_exchangeRates()
 {
 	std::cout << GREEN << "BitcoinExchange: Default constructor called" << RESET << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(std::string &filename)
+{
+	std::cout << GREEN << "BitcoinExchange: Parameter constructor called" << RESET << std::endl;
+
+	parseData(filename);
 }
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &rhs)
@@ -34,13 +43,16 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange &rhs)
 	*this = rhs;
 }
 
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &rhs)
+BitcoinExchange &
+BitcoinExchange::operator=(const BitcoinExchange &rhs)
 {
 	std::cout << GREEN << "BitcoinExchange: Assignment operator called" << RESET << std::endl;
 
 	if (this != &rhs)
 	{
 		// Perform deep copy
+
+		_exchangeRates = rhs._exchangeRates;
 	}
 
 	return (*this);
@@ -52,7 +64,89 @@ BitcoinExchange::~BitcoinExchange(void)
 }
 
 // ************************************************************************** //
-//                                Public methods                              //
+//                               Adding rates                                //
 // ************************************************************************** //
+
+void
+BitcoinExchange::addRate(std::string date, double rate)
+{
+	_exchangeRates[date] = rate;
+}
+
+void
+BitcoinExchange::parseData(std::string &filename)
+{
+	// Parse the file and add the rates to the map
+	try
+	{
+		// Open the file
+		std::string		line;
+		std::ifstream	file(filename);
+
+		if (file.good() == false || file.is_open() == false)
+			throw (std::ifstream::failure("Error: could not open file"));
+
+		do
+		{
+			// Read the file
+			std::getline(file, line);
+
+			// Parse the line
+			std::string		date = line.substr(0, line.find(','));
+			double			rate = std::stod(line.substr(line.find(',') + 1));
+
+			// Add the rates to the map
+			addRate(date, rate);
+		}
+		while (file.good() == true);
+
+		// Close the file
+		file.close();		
+	}
+	catch (const std::invalid_argument &e)
+	{
+		std::cout << RED << "Exception caught: Invalid argument for function - " << e.what() << RESET << std::endl;
+	}
+	catch (const std::out_of_range &e)
+	{
+		std::cout << RED << "Exception caught: Rate is out of range for function - " << e.what() << RESET << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << RED << e.what() << RESET << std::endl;
+	}
+}
+
+// ************************************************************************** //
+//                               Public Methods                              //
+// ************************************************************************** //
+
+double
+BitcoinExchange::getRate(std::string &date) const
+{
+	// Find the rate for the given date
+	std::map<std::string, double>::const_iterator		it;
+	
+	it = _exchangeRates.find(date);
+
+	if (it != _exchangeRates.end())
+		return (it->second);
+	else
+		throw (BitcoinExchange::NoRateAvailable());
+
+	return (0);
+}
+
+double
+BitcoinExchange::exchange(std::string &date, double amount) const
+{
+	// Exchange the given amount for the given date
+	double	rate = getRate(date);
+
+	if (amount < 0 || amount > 1000)
+		throw (BitcoinExchange::invalidValue());
+
+	return (amount * rate);
+}
 
 
